e87b2330a9cc0dea0b563b34be004f94a208669d
Fatih Deniz <deniz.fatih@hotmail.de>
2023-06-19 23:25:20 +0200

    Generic repository

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace DataAccess.Abstract
+{
+    public interface IEntityRepository<T>
+    {
+
+        List<T> FindAll(Expression<Func<T, bool>> filter = null);
+        T Find(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+    }
+}
