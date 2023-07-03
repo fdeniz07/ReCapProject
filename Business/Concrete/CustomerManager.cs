6238ed4523e069d87fbe5b56b0d4bc150fb0867e
Fatih Deniz <deniz.fatih@hotmail.de>
2023-07-04 01:24:14 +0200

    Day 10 - Adding customer entity,dal,bll

---
+++ Business/Concrete/CustomerManager.cs
@@ -0,0 +1,49 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+
+namespace Business.Concrete
+{
+    public class CustomerManager : ICustomerService
+    {
+        private ICustomerDal _customerDal;
+
+        public CustomerManager(ICustomerDal customerDal)
+        {
+            _customerDal = customerDal;
+        }
+
+
+        public IResult Add(Customer customer)
+        {
+            _customerDal.Add(customer);
+            return new SuccessResult(Messages.CUSTOMER_ADDEED);
+        }
+
+        public IResult Delete(Customer customer)
+        {
+            _customerDal.Delete(customer);
+            return new SuccessResult(Messages.CUSTOMERS_DELETED);
+        }
+
+        public IDataResult<List<Customer>> GetAll()
+        {
+            return new SuccessDataResult<List<Customer>>(_customerDal.GetAll(), Messages.CUSTOMERS_LISTED);
+        }
+
+        public IDataResult<Customer> GetById(int id)
+        {
+            return new SuccessDataResult<Customer>(_customerDal.Get(c=>c.Id==id),String.Format(Messages.CUSTOMER_LISTED, id));
+        }
+
+        public IResult Update(Customer customer)
+        {
+            _customerDal.Update(customer);
+            return new SuccessResult(Messages.CUSTOMER_UPDATED);
+        }
+    }
+}
